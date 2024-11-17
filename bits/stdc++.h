3340608d7555f1bf3f054b397343966aaaad7e38
gucci1909 <umangarora0134@gmail.com>
2024-11-18 03:31:25 +0530

    stl added

---
+++ bits/stdc++.h
@@ -0,0 +1,17 @@
+#ifndef _GLIBCXX_BITS_STDCPP_H
+#define _GLIBCXX_BITS_STDCPP_H
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <cmath>
+#include <numeric>
+#include <cstring>
+#include <string>
+#include <cassert>
+
+#endif
